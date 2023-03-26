2fcfa7f00a712a3a39582c73e4e76b36960a8370
ilya knyazev <ilak9081@gmail.com>
2023-03-26 10:55:54 +0400

    p2

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
-   int birth;
-   int currentyear;   
-   int age;
-birth=1990;  currentyear=2010;
-age=currentyear - birth;
-printf("%d",age);
+double katet_a; double katet_b; double perimetr; double ploschad;
+katet_a=10.0; katet_b=5.0;
+perimetr=sqrt(powf(katet_a,2)+powf(katet_b,2))+katet_a+katet_b;
+printf("Perimeter is ""%f\n",perimetr);
+ploschad=(katet_a*katet_b)/2;
+printf("Area is ""%f",ploschad);
 }
